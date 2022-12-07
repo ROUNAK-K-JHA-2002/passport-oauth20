91a1dc4772bbd47dc6f170cf1b99bfd3d9ea299f
ROUNAK KUMAR JHA <rounakjha291202.rar@gmail.com>
2022-12-07 11:51:13 +0530

    Improved Auth Control

--- index.js
+++ index.js
@@ -1,38 +1,63 @@
+
+//Importing Modules
 const express =  require('express');
 const passport = require('passport');
-require('./auth')
-require('dotenv').config();
-
-
-
+const authStrategy = require('passport-oauth2').Strategy
 const app = express();
-const port = 5000;
 
 
+
+//Declearing variables
+const port = 3000;
+const clientID = 'sampleclient';
+const clientSecret = 'clientSecret';
+let token ='sampletoken'
+var redirectUri = `http://127.0.0.1:${port}/auth/tala/callback`
+var authorizationURL = `http://b.pinggy.io:8081/oauth2/authorize?response_type=code&client_id=${clientID}&redirect_uri=${redirectUri}&scope=openid`
+var tokenUri = `http://b.pinggy.io:8081/oauth2/token?client_id=${clientID}&client_secret=${clientSecret}&grant_type=authorization_code&code=${token}&redirect_uri=${redirectUri}`
+
+
+//Fuction to check if user is logged in
 function isloggedin(req,res){
     req.user ? next() : res.sendStatus(401)
 }
 
-
-app.get('/protected',isloggedin,(req,res)=>{
+//Routes
+app.get('/protected', isloggedin ,(req,res)=>{
     res.send("Hello");
+})
+app.get('/auth/tala',passport.authenticate('oauth2',{scope : ['openid']}))
+
+app.get('/auth/tala/callback',
+    (req,res)=>{
+         token = req.query.code;
+         tokenUri = `http://b.pinggy.io:8081/oauth2/token?client_id=${clientID}&client_secret=${clientSecret}&grant_type=authorization_code&code=${req.query.code}&redirect_uri=${redirectUri}`
+         console.log(tokenUri)
+    },passport.authenticate('oauth2'),(req,res)=>{
+    console.log("Login Successful")
+    res.redirect('/protected')
 })
 
 
-app.get('/auth/tala',passport.authenticate('oauth2',{
-    scope : ['openid']
-}))
+//Connection with Auth URL
 
-app.get('/auth/tala/callback',passport.authenticate('oauth2',{
-    failureRedirect : '/auth/failed',
-    successRedirect : '/protected'
-}))
-
-
+passport.use(new authStrategy({
+    clientID : clientID,
+    clientSecret : clientSecret,
+    authorizationURL : authorizationURL,
+    tokenURL : tokenUri,
+    callbackURL : redirectUri
+},
+function(accessToken, refreshToken, profile, done) {
+    console.log(accessToken)
+    return done(err,user)
+  }))
 
 
 app.get('/',(req , res)=>{
     res.send('<a href="/auth/tala">Authenticate </a>')
 })
 
-app.listen(port,()=> console.log("Server running on http://localhost:5000"))
+app.listen(port,()=> console.log("Server running on http://127.0.0.1:3000"))
+
+
